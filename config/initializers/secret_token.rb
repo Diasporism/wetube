e63f175f72e1872824c75ef654d9114d49e1b132
Logan Sears <lsears322@gmail.com>
2013-06-10 14:59:26 -0600

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wetube::Application.config.secret_token = 'b2213847869e21c7bff1fa7d3633c253bcc0361c8705789de0dae4185bf2d0e343856638a9e3a5369b0e69354533094c575a373c6a5d9552fbb2fe942d79f422'
