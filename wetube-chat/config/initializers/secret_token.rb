c3e55c474e7c59a5f9d2230fcb09cce4d06f24ea
Geoff Schorkopf <gschorkopf@gmail.com>
2013-06-11 16:01:00 -0600

    added chat service with rspec

---
+++ wetube-chat/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WetubeChat::Application.config.secret_token = '0de93c8848f4a3b1a227b48d39d49e013ac100fa1bcbf9e38bd95705781a0ecb4370c16c9a2159b1ba40f7cf0d0a7723b70fb840c20cb5919c2ba25401f2469c'
