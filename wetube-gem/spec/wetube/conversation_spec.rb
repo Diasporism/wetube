17e9a38691a2ccb8accb5606ad517540444bff84
Geoff Schorkopf <gschorkopf@gmail.com>
2013-06-11 19:14:34 -0600

    Conversation renamed

--- wetube-gem/spec/wetube/conversation_spec.rb
+++ wetube-gem/spec/wetube/conversation_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe Wetube::Conversation do
   describe ".find" do
     xit "returns a hashie of found conversation" do
-      VCR.use_cassette('found_door') do
+      VCR.use_cassette('found_conversation') do
         result = described_class.find(1)
         expect(result.id).to eq 1
         # This will fail. There is no conversation with id 1
