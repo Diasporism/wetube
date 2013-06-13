d3152b1b88dcced266249e307395d5696ed6ae56
Paul <pnblackwell@gmail.com>
2013-06-13 17:50:19 -0600

    jquery

---
+++ wetube-frontend/app/assets/javascripts/theatre.js
@@ -0,0 +1,41 @@
+// $(function() {
+//   loadItems = function(options) {
+//     var url = options.url;
+//     var destination = options.destination;
+//     var template = options.template;
+
+//     destination.html("Loading your big set of data ****");
+
+//     $.getJSON( url, {}, function(data, textStatus, jqXHR) {
+//       destination.html("");
+
+//       jQuery.each(data,function(index, element) {
+//         var source = template.html();
+//         var feedItemTemplate = Handlebars.compile(source);
+//         var htmlToAdd = feedItemTemplate(element);
+//         destination.append(htmlToAdd);
+//       });
+//     } );
+//   };
+// });
+
+$(function() {
+  postItems = function(options) {
+    var url = options.url;
+    var destination = options.destination;
+    var template = options.template;
+
+    destination.html("Loading your big set of data ****");
+
+    $.getJSON( url, {}, function(data, textStatus, jqXHR) {
+      destination.html("");
+
+      jQuery.each(data,function(index, element) {
+        var source = template.html();
+        var feedItemTemplate = Handlebars.compile(source);
+        var htmlToAdd = feedItemTemplate(element);
+        destination.append(htmlToAdd);
+      });
+    } );
+  };
+});
