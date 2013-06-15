1cfb45a4cbc6c2fe511384915b6218ed8d69371f
Paul <pnblackwell@gmail.com>
2013-06-15 17:02:18 -0600

    Updated version, removed routes thing, built new gem

--- wetube-chat/config/routes.rb
+++ wetube-chat/config/routes.rb
@@ -1,6 +1,5 @@
 WetubeChat::Application.routes.draw do
   resources :conversations, only: [:show, :create] do
     resources :messages, only: [:create]
-    resources
   end
 end
--- wetube-gem/spec/wetube/theater_spec.rb
+++ wetube-gem/spec/wetube/theater_spec.rb
@@ -2,10 +2,10 @@ require 'spec_helper'
 
 describe Wetube::Theater do
   describe ".create" do
-    # before :each do
-    #   created_components = ["{\"id\":2}", "{\"id\":1}"]
-    #   Wetube::Theater.stub(:gather_theater_components).and_return(created_components)
-    # end
+    before :each do
+      created_components = ["{\"id\":2}", "{\"id\":1}"]
+      Wetube::Theater.stub(:gather_theater_components).and_return(created_components)
+    end
 
     it "should create a conversation" do
       result = described_class.create
