ce9ed427aca48baab388ebd1f20a0b0fc9a30940
Geoff Schorkopf <gschorkopf@gmail.com>
2013-06-16 15:52:17 -0600

    Removed databases from git

--- wetube-frontend/app/controllers/messages_controller.rb
+++ wetube-frontend/app/controllers/messages_controller.rb
@@ -1,5 +1,9 @@
 class MessagesController < ApplicationController
   def create
-    @message = Wetube::Conversation.create_message(params[:conversation_id], params[:message])
+    @message = Wetube::Conversation.create_message(
+      params[:conversation_id],
+      {content: params[:content],
+        user_id: 1})
+    # raise @message.inspect
   end
 end
