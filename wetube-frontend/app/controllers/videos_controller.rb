3d6f315292c46bfe98d2a0a457412fa87b71b02b
Geoff Schorkopf <gschorkopf@gmail.com>
2013-06-17 00:14:40 -0600

    Localized subscriptions to per theater basis

--- wetube-frontend/app/controllers/videos_controller.rb
+++ wetube-frontend/app/controllers/videos_controller.rb
@@ -1,5 +1,6 @@
 class VideosController < ApplicationController
   def create
+    @theater_id = Theater.find_by_playlist_id(params[:playlist_id]).id
     @video = Wetube::Playlist.find_or_create_video(
       params[:playlist_id],
       { url: params[:url],
