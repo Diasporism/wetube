edd9d54cf6759298e757bc014e48fe44480ff8e9
Geoff Schorkopf <gschorkopf@gmail.com>
2013-06-24 11:23:15 -0600

    Added helper method to populate home page with seed images, updates to home show CSS

--- wetube-frontend/app/controllers/application_controller.rb
+++ wetube-frontend/app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  helper_method :seed_playlists
+
+  def seed_playlists
+    {
+      "james-blakes-greatest-hits" => "home-filler.jpg",
+      "james-blakes-b-sides" => "home-filler.jpg",
+      "james-blakes-eponymous-first-try" => "home-filler.jpg",
+      "music-from-james-blakes-eps" => "home-filler.jpg",
+      "james-blakes-djing-prowess" => "home-filler.jpg"
+    }
+  end
+
 end
--- wetube-frontend/app/controllers/home_controller.rb
+++ wetube-frontend/app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def show
     @theaters = Theater.all
-    # @playlists = seed_playlists
+    @playlists = seed_playlists
   end
 end
