faf9ac63a2a97781aa63ee8d3a7d37d39e90b427
Kyle Suss <susskyle@gmail.com>
2013-07-02 15:18:26 -0600

    added the icons for play and pause as well as added client side validation for the chat input

--- wetube-playlist/db/schema.rb
+++ wetube-playlist/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130625235621) do
+ActiveRecord::Schema.define(:version => 20130702003058) do
 
   create_table "conversations", :force => true do |t|
     t.datetime "created_at", :null => false
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(:version => 20130625235621) do
     t.datetime "created_at",      :null => false
     t.datetime "updated_at",      :null => false
     t.integer  "conversation_id"
+    t.string   "name"
   end
 
   create_table "playlist_videos", :force => true do |t|
